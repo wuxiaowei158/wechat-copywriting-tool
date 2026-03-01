b0d7c26059919ff69e909577b1ac200bb79c1264
武小卫 <452799386@qq.com>
2026-03-01 02:30:07 +0000

    add src/app/page.tsx.
    Signed-off-by: 武小卫 <452799386@qq.com>

---
+++ src/app/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ padding: '20px', textAlign: 'center' }}>
+      <h1>微信公众号文案仿写工具</h1>
+      <p>项目已成功部署到 Vercel！</p>
+    </div>
+  );
+}
